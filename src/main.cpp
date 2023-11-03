3c7a966247035994ba7ddbf6b92fcb354670ab08
Pierre Thomain <pthomain@gmail.com>
2023-11-03 19:41:19 +0000

    Adding scope transformations

--- src/main.cpp
+++ src/main.cpp
@@ -2,34 +2,52 @@
 #include <effects.h>
 #include <leds.h>
 
+#define FRAMES_PER_SECOND 30
+
+uint8_t lastEffect = -1;
+uint8_t effect = 0;
+Scope scope;
+Display *display;
+
+void updateEffectIfChanged(const Effect &effect);
+
 void setup() {
     Serial.begin(9600);
-    delay(1500);
-    initLeds();
+    delay(1000);
+    display = initDisplay();
+
+    set_max_power_in_volts_and_milliamps(5, 500);
+    FastLED.clear();
+    FastLED.show();
 }
 
 void loop() {
-//    CRGBPalette16 palette16;
-//    int delayMs = 2000;
-//
-//    EVERY_N_MILLISECONDS(delayMs) {
-//        palette16 = CRGBPalette16(
-//                CHSV(random8(), 255, random8(128, 255)),
-//                CHSV(random8(), 255, random8(128, 255)),
-//                CHSV(random8(), 192, random8(128, 255)),
-//                CHSV(random8(), 255, random8(128, 255))
-//        );
-//    };
-//
-//    EVERY_N_MILLISECONDS(delayMs)
-//    {
-//        applyEffect(
-//                Effect(
-//                        SCOPE_WHOLE,
-//                        LTR,
-//                        DIVISION_WHOLE,
-//                        palette16
-//                )
-//        );
+//    FastLED.delay(1000/FRAM
+//    ES_PER_SECOND);
+
+
+    updateEffectIfChanged(Effect(RainbowColors_p));
+//    EVERY_N_MILLIS(5000) {
+//        effect += 1;
+//    }
+}
+
+void updateEffectIfChanged(const Effect &effect2) {
+//    if (lastEffect != effect) {
+
+    EVERY_N_SECONDS(5) { effect++; };
+
+    if (effect % 3 == 0) {
+        scope = SCOPE_WHOLE;
+    } else if (effect % 3 == 1) {
+        scope = SCOPE_WORD;
+    } else {
+        scope = SCOPE_LETTER;
+    }
+
+    display->render(
+            effect2,scope
+    );
+//    lastEffect = effect;
 //    }
 }
--- src/palettes.h
+++ src/palettes.h
@@ -1,40 +1,43 @@
 #pragma once
 
 #include <FastLED.h>
-#include <leds.h>
+#include <utils.h>
 
 uint16_t x = 0;
 uint16_t time = 0;
 uint8_t scale = 40;
 uint8_t octave = 1;
-uint8_t noiseData[NUM_LETTERS];
 
-int8_t normaliseNoise(int8_t value) {
+uint8_t normaliseNoise(int8_t value) {
     return map(value, 30, 195, 0, 255);
 }
 
-void showNoisePalette(CRGBPalette16 palette) {
-    EVERY_N_MILLISECONDS(100)
-    {
-        Serial.println("tick");
-
-        time = millis() / 10;
-        x = beatsin8(11, 0, NUM_LETTERS);
-
-        memset(noiseData, 0, NUM_LETTERS);
-        fill_raw_noise8(noiseData, NUM_LETTERS, octave, x, scale, time);
-        Serial.println("tock");
-
-        for (int8_t i = 0; i < NUM_LEDS; i++) {
-            auto colour = ColorFromPalette(
-                    palette,
-                    normaliseNoise(noiseData[i]),
-                    255
-            );
-            leds[i] = colour;
-        }
-        Serial.println("tack");
-
-        FastLED.show();
-    }
-}
+//void showNoisePalette(CRGBPalette16 palette) {
+//    uint8_t noiseData[NUM_LEDS];
+//    memset8(noiseData, 0, NUM_LEDS);
+//
+//    EVERY_N_MILLISECONDS(100)
+//    {
+//        time = millis() / 10;
+//        printNumber("Time", time);
+//
+//        x = beatsin8(11, 0, NUM_LEDS);
+//        printNumber("X", time);
+//
+//        fill_raw_noise8(noiseData, NUM_LEDS, octave, x, scale, time);
+//        Serial.println("Noise filled");
+//
+//        for (uint8_t i = 0; i < 10; i++) {
+//            printNumber("Calculate pixel colour at pos", i);
+//            auto colour = ColorFromPalette(
+//                    palette,
+//                    normaliseNoise(noiseData[i]),
+//                    255
+//            );
+//            ledWhole[i] = colour;
+//        }
+//
+//        Serial.println("Show noise");
+//        FastLED.show();
+//    }
+//}

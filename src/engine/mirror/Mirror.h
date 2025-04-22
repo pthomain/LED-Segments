5b5c015a2d775137b8e278ff9cdecc3985d96c8a
Pierre Thomain <pthomain@users.noreply.github.com>
2025-04-22 14:10:13 +0100

    Adding License

--- src/engine/mirror/Mirror.h
+++ src/engine/mirror/Mirror.h
@@ -1,10 +1,47 @@
+//  SPDX-License-Identifier: GPL-3.0-or-later
+//  Copyright (C) 2023 Pierre Thomain <pthomain@gmail.com>
+//
+// /*
+//  * This file is part of LED Segments.
+//  *
+//  * LED Segments is free software: you can redistribute it and/or modify
+//  * it under the terms of the GNU General Public License as published by
+//  * the Free Software Foundation, either version 3 of the License, or
+//  * (at your option) any later version.
+//  *
+//  * LED Segments is distributed in the hope that it will be useful,
+//  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//  * GNU General Public License for more details.
+//  *
+//  * You should have received a copy of the GNU General Public License
+//  * along with LED Segments. If not, see <https://www.gnu.org/licenses/>.
+//  *
+//
+
+/*
+ * This file is part of LED Segments.
+ *
+ * LED Segments is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * LED Segments is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with LED Segments. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #pragma once
 
 #ifndef LED_SEGMENTS_MIRROR_H
 #define LED_SEGMENTS_MIRROR_H
 
 #include <vector>
-#include <string>
 #include "FastLED.h"
 
 typedef enum {

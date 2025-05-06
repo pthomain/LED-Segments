01765df587bc375fbc8f24c2c6215becba0de42d
Pierre Thomain <pthomain@users.noreply.github.com>
2025-05-06 11:02:48 +0100

    Refactoring transitions as effects

--- src/specs/phrase/PhraseSpec.cpp
+++ src/specs/phrase/PhraseSpec.cpp
@@ -1,5 +1,5 @@
 //  SPDX-License-Identifier: GPL-3.0-or-later
-//  Copyright (C) 2023 Pierre Thomain <pthomain@gmail.com>
+//  Copyright (C) 2023 Pierre Thomain
 
 /*
  * This file is part of LED Segments.
